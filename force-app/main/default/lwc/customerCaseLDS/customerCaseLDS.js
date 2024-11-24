133139a4a8bb5a12c923c8f1d0c33a128eedd0f3
IlliaKarabanov <IlliaKarabanov@gmail.com>
2024-11-24 13:40:45 +0300

    add: incomingCallLWC

---
+++ force-app/main/default/lwc/customerCaseLDS/customerCaseLDS.js
@@ -0,0 +1,190 @@
+import { LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getContact from '@salesforce/apex/CustomerCaseController.getContact';
+import createContact from '@salesforce/apex/CustomerCaseController.createContact';
+import createCase from '@salesforce/apex/CustomerCaseController.createCase';
+
+import { createRecord  } from 'lightning/uiRecordApi';
+
+import CASE_OBJECT from '@salesforce/schema/Case';
+import SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
+import SUPPLIED_PHONE_FIELD from '@salesforce/schema/Case.SuppliedPhone';
+import SUPPLIED_EMAIL_FIELD from '@salesforce/schema/Case.SuppliedEmail';
+import CONTACT_FIELD from '@salesforce/schema/Case.ContactId';
+
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
+
+export default class InputGuest extends LightningElement {
+
+    contactId = null;
+    firstName = '';
+    lastName = '';
+    email = '';
+    phone = '';
+    message = '';
+
+    firstNameHintVisible = false;
+    lastNameHintVisible = false;
+    emailHintVisible = false;
+    messageHintVisible = false;
+    disableSave = false;
+
+
+    async sendData(){
+
+        let contacts = [];
+        const contactFields = {};
+        contactFields[FIRST_NAME_FIELD.fieldApiName] = this.firstName;
+        contactFields[LAST_NAME_FIELD.fieldApiName] = this.lastName;
+        contactFields[EMAIL_FIELD.fieldApiName] = this.email;
+        contactFields[PHONE_FIELD.fieldApiName] = this.phone;
+        const contactRecordInput = {apiName: CONTACT_OBJECT.objectApiName, fields: contactFields};
+
+        try {
+            contacts = await getContact({firstName: this.firstName, lastName: this.lastName, email: this.email})
+        } 
+        catch(error) {
+            console.log('1');
+            console.log(error);
+            this.dispatchEvent(new ShowToastEvent({title: 'ERROR', variant: 'error', message: error.message})); return}
+        
+        if(contacts.length === 0){
+            try {     
+                const cont = await createRecord(contactRecordInput);
+                this.contactId = cont.id;
+            }  
+            catch(error) {
+                console.log('2');
+                console.log(error);
+                this.dispatchEvent(new ShowToastEvent({title: 'ERROR', variant: 'error', message: error.message})); return}
+            }
+        else   
+            this.contactId = contacts[0].Id;
+ 
+
+        const caseFields = {};
+        caseFields[STATUS_FIELD.fieldApiName] = 'New';
+        caseFields[ORIGIN_FIELD.fieldApiName] = 'Web';
+        caseFields[SUBJECT_FIELD.fieldApiName] = this.message;
+        caseFields[SUPPLIED_PHONE_FIELD.fieldApiName] = this.phone;
+        caseFields[SUPPLIED_EMAIL_FIELD.fieldApiName] = this.email;
+        caseFields[CONTACT_FIELD.fieldApiName] = this.contactId;
+        const caseRecordInput = {apiName: CASE_OBJECT.objectApiName, fields: caseFields};
+
+        try {        
+            await createRecord(caseRecordInput);
+            //await createCase({subject: this.message, suppliedPhone: this.phone, suppliedEmail: this.email, contactId: this.contactId});
+            this.dispatchEvent(new ShowToastEvent({title: 'Message Sent', variant: 'success'}))} 
+        catch (error) {
+            this.dispatchEvent(new ShowToastEvent({title: 'ERROR', variant: 'error', message: error.message}))}
+    }
+
+
+    async sendData(){
+
+        let contacts = [];
+
+        try {
+            contacts = await getContact({firstName: this.firstName, lastName: this.lastName, email: this.email})} 
+        catch(error) {
+            this.dispatchEvent(new ShowToastEvent({title: 'ERROR', variant: 'error', message: error.message})); return false}
+        
+        if(contacts.length === 0){
+            try {     
+                this.contactId = await createContact({firstName: this.firstName, lastName: this.lastName, email: this.email, phone: this.phone});}  
+            catch(error) {
+                this.dispatchEvent(new ShowToastEvent({title: 'ERROR', variant: 'error', message: error.message})); return false}}
+        else   
+            this.contactId = contacts[0].Id;
+
+        try {        
+            await createCase({subject: this.message, suppliedPhone: this.phone, suppliedEmail: this.email, contactId: this.contactId});
+            this.dispatchEvent(new ShowToastEvent({title: 'Message Sent', variant: 'success'})) 
+            return false}
+        catch (error) {
+            this.dispatchEvent(new ShowToastEvent({title: 'ERROR', variant: 'error', message: error.message}))}
+
+        return true;
+    }
+
+    errorsExist(){
+
+        let errorExist = false;
+        const hints = this.template.querySelectorAll('[data-name="error-hint"]');
+        hints.forEach(el => {
+            const errEx = this.operateInput(el);
+            if(errEx) errorExist = true;
+        });
+
+        return errorExist;
+    }
+    
+    operateInput(el){
+
+        let errorExist = false;
+
+        const hintName = el.getAttribute('data-id').replace('-hint', '');
+        const elName = el.getAttribute('data-id').replace('-hint', '');
+        const hintBox = this.template.querySelector(`[data-id="${hintName}-hint-box"]`); 
+        if(this[elName].trim()){  
+            el.classList.add('slds-hidden'); 
+            el.classList.remove('slds-show');              
+            hintBox.classList.remove("slds-has-error");
+        }
+        else{
+            el.classList.remove('slds-hidden'); 
+            el.classList.add('slds-show');
+            hintBox.classList.add("slds-has-error");
+            errorExist = true;
+        }
+
+         return errorExist;
+    }
+
+    async handleSave(event) {     
+
+        if(!this.errorsExist()){
+            this.disableSave = true;
+            const isSent = await this.sendData();      
+            this.disableSave = isSent;
+        }      
+    }
+ 
+    onChangeFirstNameHandler(event) {
+        this.firstName = event.target.value;
+
+        if(this.firstName.trim())
+            this.operateInput(this.template.querySelector('[data-id="firstName-hint"]'));
+    }
+
+    onChangeLastNameHandler(event) {
+        this.lastName = event.target.value;
+
+        if(this.firstName.trim())
+            this.operateInput(this.template.querySelector('[data-id="firstName-hint"]'));
+    }
+   
+    onChangeEmailHandler(event) {
+        this.email = event.target.value;
+
+        if(this.firstName.trim())
+            this.operateInput(this.template.querySelector('[data-id="firstName-hint"]'));
+    }
+
+    onChangePhoneHandler(event) {
+        this.phone = event.target.value;
+    }
+
+    onChangeMessageHandler(event) {
+        this.message = event.target.value;
+
+        if(this.firstName.trim())
+            this.operateInput(this.template.querySelector('[data-id="firstName-hint"]'));
+    }
+
+    
+}
---
+++ force-app/main/default/lwc/incomingCallLwc/incomingCallLwc.js
@@ -0,0 +1,232 @@
+import { LightningElement, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import { encodeDefaultFieldValues } from "lightning/pageReferenceUtils";
+import unknown_Caller from "@salesforce/label/c.Unknown_Caller";
+import found_Accounts from "@salesforce/label/c.Found_Accounts";
+import anonymous from "@salesforce/label/c.Anonymous";
+import { NavigationMixin } from "lightning/navigation";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import New_Personal_Account from "@salesforce/label/c.New_Personal_Account";
+import New_Business_Contact from "@salesforce/label/c.New_Business_Contact";
+import New_Account from "@salesforce/label/c.New_Account";
+import Search_Existing_Accounts_Contacts from "@salesforce/label/c.Search_Existing_Accounts_Contacts";
+import Found_Accounts from "@salesforce/label/c.Found_Accounts";
+import Found_Contacts from "@salesforce/label/c.Found_Contacts";
+
+import getAccountsContacts from "@salesforce/apex/IncomingCallController.getAccountsContacts";
+import getBusinessRecordTypeId from "@salesforce/apex/IncomingCallController.getBusinessRecordTypeId";
+
+
+export default class IncomingCallLwc extends NavigationMixin(LightningElement) {
+
+    newPersonalAccount = New_Personal_Account;
+    newBusinessAccount = New_Business_Contact;
+    newAccount = New_Account;
+    searchExistingAccountsContacts = Search_Existing_Accounts_Contacts;
+    foundAccounts = Found_Accounts;
+    foundContacts = Found_Contacts;
+
+    pnr = '';
+    accountData = [];
+    contactData = [];
+    accountColumns = [];
+    contactColumns = [];
+    businessRecordTypeId = null;
+    searchStr = '';
+    isOpenModal = false;
+    searchAndCreateVisible = false;
+    accountTableVisible = false;
+    contactTableVisible = false;
+
+    @wire(CurrentPageReference)
+    getStateParameters(currentPageReference) {
+       if (currentPageReference)
+          this.initLoad(currentPageReference.state?.c__pnr);
+    }
+
+    async initLoad(pnr){
+        this.clearData(pnr);
+    	this.setPhoneFromURL(pnr);
+        this.setContactColumns();
+        this.setAccountColumns();
+        await this.getAccountsContacts({fields: 'PHONE FIELDS', searchString: this.pnr});
+        this.operateFirstLoad();
+    }
+
+    clearData(){
+        this.pnr = '';
+        this.accountData = [];
+        this.contactData = [];
+        this.accountColumns = [];
+        this.contactColumns = [];
+        this.businessRecordTypeId = null;
+        this.searchStr = '';
+        this.isOpenModal = false;
+        this.searchAndCreateVisible = false;
+        this.accountTableVisible = false;
+        this.contactTableVisible = false;
+    }
+
+    setPhoneFromURL(pnr){ 
+        this.pnr = this.parsePhoneFromURL(pnr);
+    }
+
+    parsePhoneFromURL(pnr){ 
+        return pnr.trim() ?? 'anonymous';
+    }
+
+    setContactColumns() {
+        this.contactColumns = [
+            {label: 'Name', 			fieldName: 'Link', 				type: 'url', 	sortable: true, 	typeAttributes: {label: {fieldName: 'Name', }, 		target: '_blank', tooltip: 'go to contact',},},        
+            {label: 'Phone', 			fieldName: 'Phone', 			type: 'phone', 	sortable: true},        
+            {label: 'Account', 			fieldName: 'AccountName', 		type: 'text', 	sortable: true}, 
+        ];
+    }
+         
+    setAccountColumns(){
+        this.accountColumns = [
+            {label: 'Name', 			fieldName: 'Link', 				type: 'url', 	typeAttributes: {label: {fieldName: 'Name', }, 		target: '_blank', tooltip: 'go to contact',},},        
+            {label: 'Phone', 			fieldName: 'Phone', 			type: 'phone',	},        
+            {label: 'Address', 			fieldName: 'BillingAddress', 	type: 'text',	},
+            {label: 'Type', 			fieldName: 'RecordTypeName', 	type: 'text',	},         
+        ];        
+    }
+
+    async getAccountsContacts(searchObj){
+        
+        if (searchObj.searchString == 'anonymous')
+            return;
+       
+        try{
+            const data = await getAccountsContacts({searchString: searchObj.searchString, searchField: searchObj.fields});
+            this.accountData = this.operateAccountFields(data[0]);
+            this.contactData = this.operateContactFields(data[1]);
+        }
+        catch(error){
+            this.dispatchEvent(new ShowToastEvent({title: 'ERROR', variant: 'error', message: error.message})); 
+        }     
+        //this.handleSpinnerToggle(cmp);
+    }
+
+
+    operateAccountFields(dataArr){
+        return dataArr.map(record => {
+            const result = Object.assign({}, record);
+            result.Link = '/' + record.Id;
+            result.BillingAddress = (record.BillingCountry ? record.BillingCountry + ', ' : '') 
+                                    + (record.BillingCity ? record.BillingCity + ', ' : '')  
+                                    + (record.BillingState ? record.BillingState + ', ' : '')
+                                    + (record.BillingStreet ? record.BillingStreet + ', ' : '')	
+                                    + (record.BillingPostalCode ? record.BillingPostalCode : '');
+            result.RecordTypeName = record.RecordType ? record.RecordType.Name : '';
+            return result;
+        });
+    }
+            
+    operateContactFields(dataArr){
+        return dataArr.map(record => {
+            const result = Object.assign({}, record);
+            result.Link = '/' + record.Id;
+            result.AccountName =  record.Account ? record.Account.Name : '';
+            return result;
+        });
+    }
+    
+    operateFirstLoad(){
+        
+        if(this.accountData.length === 0 && this.contactData.length === 0){
+            this.isOpenModal = true;  
+            this.searchAndCreateVisible = true;
+            this.getBusinessRecordTypeId();
+        }
+        else if(this.accountData.length <= 1 && this.contactData.length <= 1){
+            const record = this.accountData.length === 0 ? this.contactData[0] : this.accountData[0];
+            this.navigateToURL('standard__recordPage', {'recordId': record.Id, actionName: "view"}); 
+        }
+        else{
+            this.isOpenModal = true;   
+            this.operateTablesVisible();
+        }     
+    }
+    
+    operateTablesVisible(){       
+        this.accountTableVisible = this.accountData.length !== 0;
+        this.contactTableVisible = this.contactData.length !== 0;     
+    }
+    
+    getBusinessRecordTypeId(){
+        getBusinessRecordTypeId()
+        .then(Id => this.businessRecordTypeId = Id)
+        .catch(error => this.dispatchEvent(new ShowToastEvent({title: 'ERROR', variant: 'error', message: error.message})));  
+    }   
+                                    
+    navigateToURL(type, attributes, state = {}) {   
+        this[NavigationMixin.Navigate]({
+            type,
+            attributes,
+            state,
+        });
+    }    
+
+    openObjectCreateForm(objectApiName, state){       
+        this.navigateToURL('standard__objectPage', {objectApiName: objectApiName, actionName: 'new'}, state);
+    }
+                
+    /*handleSpinnerToggle(){
+        //$A.util.toggleClass(cmp.find('spinner'), 'slds-hide');
+    }
+
+    handleNewAccountButton(){
+        navigateToURL('standard__objectPage', {objectApiName: 'Account', actionName: 'new'}, {phone: this.pnr, recordTypeId: this.businessRecordTypeId});
+    }*/
+
+    get callerLabel(){
+        return this.searchAndCreateVisible ? unknown_Caller : found_Accounts;
+    }
+
+    get phoneLabel(){
+        return this.pnr === 'anonymous' ? anonymous : this.pnr;
+    }
+
+    handleModalCancel(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__namedPage',
+            attributes: {
+                pageName: 'home'
+            },
+        });
+    }
+
+    handleNewPersonAccountButton(){
+        this.openObjectCreateForm('Account', 
+                                    {defaultFieldValues: encodeDefaultFieldValues({Phone: this.pnr})});
+    }
+
+    handleNewAccountButton(){
+        this.openObjectCreateForm(
+            'Account', 
+            {
+                defaultFieldValues: encodeDefaultFieldValues({Phone: this.pnr}),
+                recordTypeId: this.businessRecordTypeId
+            });
+    }
+
+    handleNewContactButton(){
+        this.openObjectCreateForm(
+            'Contact', 
+            {defaultFieldValues: encodeDefaultFieldValues({Phone: this.pnr})});
+    }
+
+    async handleSearch(event){ 
+        this.accountData = [];
+        this.contactData = [];
+
+        const searchStr = event.target.value;
+
+        if(searchStr && searchStr.length >= 2)
+        	await this.getAccountsContacts({fields: 'NAME FIELDS', searchString: searchStr});
+
+        this.operateTablesVisible();
+    }
+}
